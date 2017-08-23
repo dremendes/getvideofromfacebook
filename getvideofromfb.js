07ea4eed9c683a84c6954d4a97ad9c029921acbb
André Mendes <andre.mendesc@gmail.com>
2017-08-23 02:58:03 -0400

    Now outputs video with page title as filename

--- getvideofromfb.js
+++ getvideofromfb.js
@@ -4,68 +4,77 @@ const fs = require('fs');
 const request = require('request');
 
 (async () => {
-	const spitLinkError = () => { console.log('Não consegui encontrar o vídeo, o link está correto?'); }
+    const spitLinkError = () => { console.log('Não consegui encontrar o vídeo, o link está correto?'); }
 
-	if(process.argv.length <= 2){
-		console.log("Insira o link da página do vídeo como argumento");
-		process.exit(-1);
-	}
+    if(process.argv.length <= 2){
+        console.log("Insira o link da página do vídeo como argumento");
+        process.exit(-1);
+    }
 
-	const browser = await puppeteer.launch();
-	try {
-		new URL(process.argv[2]);
-	} catch (err) {
-		spitLinkError()
-		browser.close();
-		process.exit(-1);
-	}
+    const browser = await puppeteer.launch();
+    try {
+        new URL(process.argv[2]);
+    } catch (err) {
+        spitLinkError()
+        browser.close();
+        process.exit(-1);
+    }
 
-	const isFromFb = (url) => {
-		return ["www.facebook.com",
-		    "facebook.com",
-		    "www.fb.com",
-		    "fb.com",
-		    "m.facebook.com",
-		    "mobile.facebook.com"
-		   ].includes(url)
-	}
+    const isFromFb = (url) => {
+        return ["www.facebook.com",
+            "facebook.com",
+            "www.fb.com",
+            "fb.com",
+            "m.facebook.com",
+            "mobile.facebook.com"
+           ].includes(url)
+    }
 
-	const link = new URL(process.argv[2]);
+    const link = new URL(process.argv[2]);
 
-	if (isFromFb(link.host)) {
-		const page = await browser.newPage();
-		await page.goto("http://m.facebook.com" + 
-						 link.href.substring(link.origin.length, link.href.length),
-						{waitUntil: 'networkidle'})
+    if (isFromFb(link.host)) {
+        const page = await browser.newPage();
+        
+        await page.goto("http://www.facebook.com" + 
+                         link.href.substring(link.origin.length, link.href.length),
+                        {waitUntil: 'networkidle'})
 
-		await page.click('#u_0_0 > div > div > div > div > div > i')
-				  .catch((error) => {
-				  	spitLinkError()
-				  	browser.close();
-				  	process.exit(-1);
-				  });
+        const outputFileName = await page.evaluate(() => {
+            return document.title.split(' ').join('-') + '.mp4';
+        });
 
-		await page.waitForSelector('#mInlineVideoPlayer', {visible: true});
+        await page.goto("http://m.facebook.com" + 
+                         link.href.substring(link.origin.length, link.href.length),
+                        {waitUntil: 'networkidle'})
 
-		// Pega o link do video da página
-		const videoLink = await page.evaluate(() => {
-			return document.getElementById('mInlineVideoPlayer').src;
-		});
+        await page.click('#u_0_0 > div > div > div > div > div > i')
+                  .catch((error) => {
+                    spitLinkError()
+                    browser.close();
+                    process.exit(-1);
+                  });
 
-		const date = new Date();
+        await page.waitForSelector('#mInlineVideoPlayer', {visible: true});
 
-		request
-			.get(videoLink, {timeout: 3000000})
-			.on('error', function(err) {
-				console.log("Erro ao baixar vídeo :(")
-			})
-			.pipe(fs.createWriteStream('video' + date.getTime().toString() + '.mp4'));
+        // Pega o link do video da página
+        const videoLink = await page.evaluate(() => {
+            return document.getElementById('mInlineVideoPlayer').src;
+        });
 
-		console.log('Video baixado em ' + 'video' + date.getTime().toString() + '.mp4')
-		
-	} else {
-		spitLinkError();
-	}
-  	
-  	browser.close();
+        const date = new Date();
+
+        request
+            .get(videoLink, {timeout: 3000000})
+            .on('error', function(err) {
+                console.log("Erro ao baixar vídeo :(")
+            })
+            .pipe(fs.createWriteStream(outputFileName));
+
+        console.log('Video baixado em ' + outputFileName)
+        
+    } else {
+        spitLinkError();
+    }
+    
+    browser.close();
 })();

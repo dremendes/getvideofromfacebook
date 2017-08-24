4ae0508f7778868e160eb089a41075d37e6ec7e2
André Mendes <andre.mendesc@gmail.com>
2017-08-23 20:10:40 -0400

    Pequeno rewrite com melhorias

--- getvideofromfb.js
+++ getvideofromfb.js
@@ -14,7 +14,13 @@ const fs = require('fs');
 const request = require('request');
 
 (async () => {
-    const spitLinkError = () => { console.log('Não consegui encontrar o vídeo, o link está correto?'); }
+    const spitLinkErrorAndExit = (defaultMessage = true) => { 
+        if(defaultMessage){
+            console.log('Não consegui encontrar o vídeo, o link está correto?'); 
+        }
+        browser.close(); 
+        process.exit(-1);
+    }
 
     if(process.argv.length <= 2){
         console.log("Insira o link da página do vídeo como argumento");
@@ -27,9 +33,7 @@ const request = require('request');
     try {
         new URL(process.argv[2]);
     } catch (err) {
-        spitLinkError()
-        browser.close();
-        process.exit(-1);
+        spitLinkErrorAndExit()
     }
 
     const isFromFb = (url) => {
@@ -44,51 +48,50 @@ const request = require('request');
 
     const link = new URL(process.argv[2]);
 
-    if (isFromFb(link.host)) {
-        const page = await browser.newPage();
-        
-        // Primeiro visita a versão desktop 
-        // para obter o título. Isso pois na
-        // versão mobile ele é suprimido
-        await page.goto("http://www.facebook.com" + 
-                         link.href.substring(link.origin.length, link.href.length),
-                        {waitUntil: 'networkidle'})
-
-        const outputFileName = await page.evaluate(() => {
-            return document.title.split(' ').join('-') + '.mp4';
-        });
-
-        // Visita a versão mobile da página
-        await page.goto("http://m.facebook.com" + 
-                         link.href.substring(link.origin.length, link.href.length),
-                        {waitUntil: 'networkidle'})
-
-        // Dá play no vídeo
-        await page.click('#u_0_0 > div > div > div > div > div > i')
-                  .catch((error) => {
-                    spitLinkError()
-                    browser.close();
-                    process.exit(-1);
-                  });
-        await page.waitForSelector('#mInlineVideoPlayer', {visible: true});
-
-        // Pega o link do video da página
-        const videoLink = await page.evaluate(() => {
-            return document.getElementById('mInlineVideoPlayer').src;
-        });
-
-        request
-            .get(videoLink, {timeout: 3000000})
-            .on('error', function(err) {
-                console.log("Erro ao baixar vídeo :(")
-            })
-            .pipe(fs.createWriteStream(outputFileName));
-
-        console.log('Video baixado em ' + outputFileName)
-        
-    } else {
-        spitLinkError();
+    if (!isFromFb(link.host)) {
+        spitLinkErrorAndExit();
     }
+
+    const page = await browser.newPage();
     
+    // Primeiro visita a versão desktop 
+    // para obter o título. Isso pois na
+    // versão mobile ele é suprimido
+    await page.goto("http://www.facebook.com" + 
+                     link.href.substring(link.origin.length, link.href.length),
+                    {waitUntil: 'networkidle'})
+
+    const outputFileName = await page.evaluate(() => {
+        return document.title.split(' ').join('-') + '.mp4';
+    });
+
+    // Visita a versão mobile da página
+    await page.goto("http://m.facebook.com" + 
+                     link.href.substring(link.origin.length, link.href.length),
+                    {waitUntil: 'networkidle'})
+
+    // Dá play no vídeo
+    await page.click('#u_0_0 > div > div > div > div > div > i')
+              .catch((error) => {
+                spitLinkErrorAndExit()
+              });
+    await page.waitForSelector('#mInlineVideoPlayer', {visible: true});
+
+    // Pega o link do video da página
+    const videoLink = await page.evaluate(() => {
+        return document.getElementById('mInlineVideoPlayer').src;
+    });
+
+    request
+        .get(videoLink, {timeout: 90000000})
+        .on('error', function(err) {
+            console.log("Erro ao baixar vídeo :(");
+            spitLinkErrorAndExit(false) //Não exibe mensagem padrão
+
+        })
+        .pipe(fs.createWriteStream(outputFileName));
+
+    console.log('Video baixado em ' + outputFileName)
+        
     browser.close();
 })();

93c759e6c7d18e60d78f9e3fb9745fba6c723f7d
André Mendes <andre.mendesc@gmail.com>
2017-08-29 01:43:51 -0400

    Adiciona download de videos do twitter, código precisa de refactoring pesado, commitando pra ver a diferença

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # getvideofromfacebook
 
-Um programa que navega no facebook para baixar vídeo de um post público.
+Um programa que navega no facebook ou twitter para baixar vídeo de um post ou tweet público.
 
 ### Instalação
 ```bash
@@ -11,16 +11,16 @@ $ yarn install
 
 ### Utilização
 ```Shell
-$ node getvideofromfb.js <link-do-video-no-facebook>
+$ node getvideofromfb.js <link-do-video-no-facebook-ou-twitter>
 ```
 
-##### Certamente seria mais simples usar a API do facebook para obter vídeos públicos; 
+##### Certamente seria mais simples usar a API do facebook ou twitter para obter vídeos públicos; 
 ##### No entando, o objetivo desse software é unicamente educativo, ele foi publicado como exemplo num [post sobre web-spiders](http://dremendes.wordpress.com/2017/08/23/web-scraping-um-spider-para-baixar-videos-do-facebook)
 
 ......
 # getvideofromfacebook
 
-A program that browses facebook to download video from a public post
+A program that browses facebook or twitter to download video from a public post or tweet
 
 ### Installation
 ```Shell
@@ -31,8 +31,8 @@ $ yarn install
 
 ### Usage
 ```bash
-$ node getvideofromfb.js <link-do-video-no-facebook>
+$ node getvideofromfb.js <link-of-post-or-tweet-with-video>
 ```
 
-##### It would certainly be easier to use facebook API to get public videos;
+##### It would certainly be easier to use facebook or twitter API to get public videos;
 ##### However, this software purpose is educational only. It was published as an example in a [post about web-spiders](http://dremendes.wordpress.com/2017/08/23/web-scraping-um-spider-para-baixar-videos-do-facebook) 
--- getvideofromfb.js
+++ getvideofromfb.js
@@ -1,6 +1,6 @@
 // 
 // Programa para obter arquivo de vídeo a partir 
-// da URL de página de vídeo do Facebook
+// da URL de página de vídeo do Facebook ou do Twitter
 // 
 // Utilização:
 // $ node getvideofromfb.js <link-do-video-no-facebook>
@@ -8,35 +8,35 @@
 // License
 // Licensed under the GNU General Public License (GPL) v3. For more information see [LICENSE]
 
-const puppeteer = require('puppeteer');
-const { URL } = require('url');
-const fs = require('fs');
+const puppeteer = require('puppeteer')
+const { URL } = require('url')
+const fs = require('fs')
 const request = require('request');
 
 (async () => {
-    const spitLinkErrorAndExit = (defaultMessage = true) => { 
+    const spitLinkErrorAndExit = async (defaultMessage = true) => { 
         if(defaultMessage){
-            console.log('Não consegui encontrar o vídeo, o link está correto?'); 
+            console.log('Não consegui encontrar o vídeo, o link está correto?')
         }
-        browser.close(); 
-        process.exit(-1);
+        await browser.close()
+        process.exit(-1)
     }
 
     if(process.argv.length <= 2){
-        console.log("Insira o link da página do vídeo como argumento");
-        process.exit(-1);
+        console.log("Insira o link da página do vídeo como argumento")
+        process.exit(-1)
     }
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch()
 
     // Valida a URL de argumento
     try {
-        new URL(process.argv[2]);
+        new URL(process.argv[2])
     } catch (err) {
         spitLinkErrorAndExit()
     }
 
-    const isFromFb = (url) => {
+    const isFromFb = async (url) => {
         return ["www.facebook.com",
             "facebook.com",
             "www.fb.com",
@@ -46,52 +46,102 @@ const request = require('request');
            ].includes(url)
     }
 
-    const link = new URL(process.argv[2]);
-
-    if (!isFromFb(link.host)) {
-        spitLinkErrorAndExit();
+    const isFromTwitter = async (url) => {
+        return ["www.twitter.com",
+            "twitter.com",
+            "mobile.twitter.com",
+            "m.twitter.com"
+            ].includes(url)
     }
 
-    const page = await browser.newPage();
-    
-    // Primeiro visita a versão desktop 
-    // para obter o título. Isso pois na
-    // versão mobile ele é suprimido
-    await page.goto("http://www.facebook.com" + 
-                     link.href.substring(link.origin.length, link.href.length),
-                    {waitUntil: 'networkidle'})
+    const downloadVideo = async (url, outputName) => {
+        return await request
+            .get(url, {timeout: 90000000})
+            .on('error', function(err) {
+                console.log("Erro ao baixar vídeo :(")
+                spitLinkErrorAndExit(false) //Não exibe mensagem padrão
+            })
+            .pipe(fs.createWriteStream(outputName));
+    }
 
-    const outputFileName = await page.evaluate(() => {
-        return document.title.split(' ').join('-') + '.mp4';
-    });
+    const link = new URL(process.argv[2])
 
-    // Visita a versão mobile da página
-    await page.goto("http://m.facebook.com" + 
-                     link.href.substring(link.origin.length, link.href.length),
-                    {waitUntil: 'networkidle'})
+    if (!(isFromFb(link.host) || isFromTwitter(link.host))) {
+        spitLinkErrorAndExit()
+    }
 
-    // Dá play no vídeo
-    await page.click('#u_0_0 > div > div > div > div > div > i')
-              .catch((error) => {
-                spitLinkErrorAndExit()
-              });
-    await page.waitForSelector('#mInlineVideoPlayer', {visible: true});
+    const page = await browser.newPage()
 
-    // Pega o link do video da página
-    const videoLink = await page.evaluate(() => {
-        return document.getElementById('mInlineVideoPlayer').src;
-    });
+    if (isFromTwitter(link.host)) {
+        // Primeiro visita a versão desktop 
+        // para obter o título. Isso pois na
+        // versão mobile ele é suprimido
+        const fixedLink = new URL("http://www.twitter.com" + 
+                                link.href.substring(link.origin.length, link.href.length) +
+                                ((link.href[link.href.length - 1] != '/') ? '/' : '') )
 
-    request
-        .get(videoLink, {timeout: 90000000})
-        .on('error', function(err) {
-            console.log("Erro ao baixar vídeo :(");
-            spitLinkErrorAndExit(false) //Não exibe mensagem padrão
-
+        await page.goto(fixedLink.href, {waitUntil: 'networkidle', networkIdleTimeout: 5000})
+        const outputFileName = await page.evaluate(() => {
+            return document.title.split(' ').join('-').split('/').join('') + '.mp4'
         })
-        .pipe(fs.createWriteStream(outputFileName));
+        // Visita a versão mobile do vídeo da página
+        await page.goto("http://mobile.twitter.com" + 
+                         fixedLink.href.substring(fixedLink.origin.length, fixedLink.href.length) + "video/1",
+                        {waitUntil: 'networkidle', networkIdleTimeout: 5000})
+        // Pega o link do video
+        const videoLink = await page.evaluate(() => {
+            try{
+                return document.getElementsByTagName('source')[0].src
+            } catch (err) {
+                return false
+            }
+        })
+        if (!videoLink){ spitLinkErrorAndExit() }
+        // Baixa e salva no disco
+        await request
+            .get(videoLink, {timeout: 90000000})
+            .on('error', function(err) {
+                console.log("Erro ao baixar vídeo :(")
+                spitLinkErrorAndExit(false) //Não exibe mensagem padrão
+            })
+            .pipe(fs.createWriteStream(outputFileName));
 
-    console.log('Video baixado em ' + outputFileName)
-        
-    browser.close();
+        console.log('Video baixado em ' + outputFileName)
+
+    } else {
+        // Primeiro visita a versão desktop 
+        // para obter o título. Isso pois na
+        // versão mobile ele é suprimido
+        await page.goto("http://www.facebook.com" + 
+                         link.href.substring(link.origin.length, link.href.length),
+                        {waitUntil: 'networkidle', networkIdleTimeout: 5000})
+        const outputFileName = await page.evaluate(() => {
+            return document.title.split(' ').join('-') + '.mp4'
+        })
+        // Visita a versão mobile da página
+        await page.goto("http://m.facebook.com" + 
+                         link.href.substring(link.origin.length, link.href.length),
+                        {waitUntil: 'networkidle', networkIdleTimeout: 5000})
+        // Dá play no vídeo
+        await page.click('#u_0_0 > div > div > div > div > div > i')
+                  .catch((error) => {
+                    spitLinkErrorAndExit()
+                  })
+        await page.waitForSelector('#mInlineVideoPlayer', {visible: true})
+        // Pega o link do video da página
+        const videoLink = await page.evaluate(() => {
+            try{
+                return document.getElementById('mInlineVideoPlayer').src
+            } catch (err) {
+                return false
+            }
+        })
+        if (!videoLink){ spitLinkErrorAndExit() }
+        // Baixa e salva no disco
+        await downloadVideo(videoLink, outputFileName)
+
+        console.log('Video baixado em ' + outputFileName)
+    }
+
+    await browser.close()
 })();
